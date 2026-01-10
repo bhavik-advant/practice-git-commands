74c4c79f356daa2c1abed61baba1812617a1f9b9
bhavik-advant <bhavik@advant.xyz>
2026-01-10 10:48:54 +0530

    another temp commit hard

--- some.js
+++ some.js
@@ -1,8 +1,12 @@
 function sum(a, b) {
     return a + b;
 }
+function sum3(a, b, c) {
+    return a + b + c;
+}
 
 console.log(sum(5, 3)); 
 console.log(sum(20, 20)); 
 console.log(sum(40, 10)); 
 console.log(sum(5, 30)); 
+console.log(sum3(5, 30, 15)); 
